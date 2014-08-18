4ce9ee37708f90de89921592b2164a8175fec2b2
Doug Clift <dougclift@yahoo.com>
2014-08-18 01:06:26 +0000

    Another icon fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -93,6 +93,4 @@ Rails.application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
-  #this will hopefully fix the bootstrap icons
-  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
 end
